863d85899e85ca0e0a844f01bfc75a06429f4bd2
rkram5424 <rkram5424@gmail.com>
2014-05-22 20:19:57 -0500

    Init commit.

---
+++ KGBerryKrunch.m
@@ -0,0 +1,11 @@
+#import <stdio.h>
+
+int main( int argc, const char *argv[] ) {
+    return 0;
+}
+
+-(NSArray) getLetterArray{
+	return ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+	'n','o','p','q','r','s','t','u','v','w','x','y','z','-','0',
+	'1','2','3','4','5','6','7','8','9']
+}
